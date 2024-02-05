5b6c405d4d75ef579613688930f55218b780464c
PoseidonBroZeus <67764617+PoseidonBroZeus@users.noreply.github.com>
2024-02-04 20:37:14 -0600

    Update script.js
    EXTENDED LORE

--- script.js
+++ script.js
@@ -13,10 +13,372 @@ const secretWordsData = [
     images: [
       "Images/E&B.jpg",
       "Images/E&BMoves.jpg",
-      "Images/Obsidian.jpg"
+      "Images/Obsidian.jpg",
+      "Images/E&B Photo.jpg"
     ],
-    textFile: "Text/eruptor_blizzard.txt"
+    textFile: "Text/Eruptor&Blizzard.txt"
   },
+  
+  {
+    word: "mighty_thimble",
+    images: [
+      "Images/Mighty Thimble Mask.jpg",
+      "Images/Might Thimble Moves.png"
+
+    ],
+    textFile: "Text/Mighty Thimble.txt"
+  },
+
+  {
+    word: "hell__stinger",
+    images: [
+      "Images/Hell Stinger.jpg",
+      "Images/WallyZinger.jpg",
+      "Images/Hell Stinger Moves.jpg"
+
+    ],
+    textFile: "Text/Hell Stinger.txt"
+  },
+
+  {
+    word: "deneye",
+    images: [
+      "Images/Deneye.jpg"
+
+
+    ],
+    textFile: "Text/Deneye.txt"
+  },
+
+  {
+    word: "buckleup",
+    images: [
+      "Images/Buckleup.jpg"
+
+
+    ],
+    textFile: "Text/Buckleup.txt"
+  },
+
+  {
+    word: "crazy_fly",
+    images: [
+      "Images/Crazy Fly.jpg",
+      "Images/Penii Penington.jpg"
+
+
+    ],
+    textFile: "Text/Crazy Fly.txt"
+  },
+
+  {
+    word: "red_moon",
+    images: [
+      "Images/Red Moon.jpg"
+
+
+    ],
+    textFile: "Text/Red Moon.txt"
+  },
+
+  {
+    word: "spearhead",
+    images: [
+      "Images/Spearhead.jpg"
+
+
+    ],
+    textFile: "Text/Spearhead.txt"
+  },
+
+  {
+    word: "hydro_flo",
+    images: [
+      "Images/Hydro Flo.jpg",
+      "Images/Sid Anderson.jpg"
+
+
+    ],
+    textFile: "Text/Hydro Flo.txt"
+  },
+
+  {
+    word: "ioncentry",
+    images: [
+      "Images/Ion centry.jpg"
+
+
+    ],
+    textFile: "Text/IonCentry.txt"
+  },
+
+  {
+    word: "owlda_lin",
+    images: [
+      "Images/Owlda Lin.jpg",
+      "Images/Owlda Lin nohat.jpg"
+
+
+    ],
+    textFile: "Text/Owlda Lin.txt"
+  },
+
+  {
+    word: "owlda_lin",
+    images: [
+      "Images/Owlda Lin.jpg",
+      "Images/Owlda Lin nohat.jpg"
+
+
+    ],
+    textFile: "Text/Owlda Lin.txt"
+  },
+
+  {
+    word: "markus_snowither",
+    images: [
+      "Images/Snowither Shades.jpg",
+      "Images/Snowither.jpg"
+
+
+    ],
+    textFile: "Text/Markus Snowither.txt"
+  },
+
+  {
+    word: "hurl_hammel",
+    images: [
+      "Images/Hammel Shades.jpg",
+      "Images/Hammel.jpg"
+
+
+    ],
+    textFile: "Text/Hurl Hammel.txt"
+  },
+
+  {
+    word: "igor_lokkdin",
+    images: [
+      "Images/Lokkdin.jpg"
+
+
+    ],
+    textFile: "Text/Igor Lokkdin.txt"
+  },
+
+  {
+    word: "linda_lokkdown",
+    images: [
+      "Images/Lokkdown.jpg"
+
+
+    ],
+    textFile: "Text/Linda Lokkdown.txt"
+  },
+
+  {
+    word: "sleepy_king",
+    images: [
+      "Images/Sleepy King.jpg"
+
+
+    ],
+    textFile: "Text/Sleepy King.txt"
+  },
+
+  {
+    word: "spiky&diky",
+    images: [
+      "Images/SpikyDiky.jpg"
+
+
+    ],
+    textFile: "Text/Spiky and Diky.txt"
+  },
+
+  {
+    word: "t",
+    images: [
+      "Images/T.jpg"
+
+
+    ],
+    textFile: "Text/T.txt"
+  },
+  
+  {
+    word: "nadocat2",
+    images: [
+      "Images/NadoCat.jpg"
+
+
+    ],
+    textFile: "Text/NadoCat2.txt"
+  },
+
+  {
+    word: "snap",
+    images: [
+      "Images/Snap.jpg"
+
+
+    ],
+    textFile: "Text/Snap.txt"
+  },
+
+  {
+    word: "windeglyde",
+    images: [
+      "Images/Windeglyde.jpg"
+
+
+    ],
+    textFile: "Text/Windeglyde.txt"
+  },
+
+  {
+    word: "badmouth",
+    images: [
+      "Images/Badmouth.jpg"
+
+
+    ],
+    textFile: "Text/Badmouth.txt"
+  },
+
+  {
+    word: "blurred_lines",
+    images: [
+      "Images/Blurred Lines Shades.jpg",
+      "Images/Blurred Lines.jpg"
+
+
+    ],
+    textFile: "Text/Blurred Lines.txt"
+  },
+
+  {
+    word: "phaser",
+    images: [
+      "Images/Phaser.jpg",
+      "Images/Phaser Photo.jpg"
+
+
+    ],
+    textFile: "Text/Phaser.txt"
+  },
+
+  {
+    word: "blockbuster",
+    images: [
+      "Images/Blockbuster.jpg"
+
+
+    ],
+    textFile: "Text/Blockbuster.txt"
+  },
+
+  {
+    word: "dynamo",
+    images: [
+      "Images/Dynamo.jpg"
+
+
+    ],
+    textFile: "Text/Dynamo.txt"
+  },
+
+  {
+    word: "jettpak",
+    images: [
+      "Images/Jettpak shades.jpg",
+      "Images/Jettpak.jpg"
+
+
+    ],
+    textFile: "Text/Jettpak.txt"
+  },
+
+  {
+    word: "mr_tetonic",
+    images: [
+      "Images/Tectonic.jpg"
+
+
+    ],
+    textFile: "Text/Tectonic.txt"
+  },
+
+  {
+    word: "beatdown",
+    images: [
+      "Images/Beatdown.jpg"
+
+
+    ],
+    textFile: "Text/Beatdown.txt"
+  },
+
+  {
+    word: "livewire",
+    images: [
+      "Images/Livewire.jpg"
+
+
+    ],
+    textFile: "Text/Livewire.txt"
+  },
+
+  {
+    word: "tumor",
+    images: [
+      "Images/Tumor Helmate.jpg",
+      "Images/Tumor.jpg"
+      
+
+
+    ],
+    textFile: "Text/Tumor.txt"
+  },
+
+  {
+    word: "codarius",
+    images: [
+      "Images/Codarius Shades.jpg",
+      "Images/Codarius.jpg"
+      
+
+
+    ],
+    textFile: "Text/Codarius.txt"
+  },
+
+  {
+    word: "taco_bueno",
+    images: [
+      "Images/Taco Bueno.jpg"
+      
+
+
+    ],
+    textFile: "Text/Taco Bueno.txt"
+  },
+
+  {
+    word: "moc",
+    images: [
+      "Images/Moc.jpg"
+      
+
+
+    ],
+    textFile: "Text/Moc.txt"
+  }
+
+  
+
+
+  
   // Add more secret words and catalogs as needed
 ];
 
